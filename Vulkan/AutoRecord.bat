dc0c2f0ee399ab1edf612f764ad71b013537bd00
NotKyloRen <31765260+NotKyloRen@users.noreply.github.com>
2018-03-09 10:27:48 +0100

    Add files via upload

--- Vulkan/AutoRecord.bat
+++ Vulkan/AutoRecord.bat
@@ -10,6 +10,9 @@ set seconds=%1
 set cubeDim=%2
 set threadCount=%3
 set output=%4
+set confNum=%5
+set initial=%6
+set testNum=%7
 
 set "exename=Vulkan"
 set "drawArg=-cubeDim %cubeDim% -cubePad 1 -threadCount %threadCount%"
@@ -31,14 +34,18 @@ logman start %exename%PerfData -as
 logman stop %exename%PerfData 
 
 REM Move files to data directory
-FOR /F "delims=_. tokens=2" %%i IN ('dir /B data_*.csv') DO call :mover %%i %output%
+FOR /F "delims=_. tokens=2" %%i IN ('dir /B data_*.csv') DO call :mover %%i %output% %confNum% %initial% %testNum%
 EXIT
 
 :mover
  set dir=data\%2\%1
+ set cnum=%3
+ set init=%4
+ set tnum=%5
  mkdir %dir%
  move perfdata*.csv %dir%\perfmon_%1.csv 
- move data_*.csv %dir%\ohmon_%1.csv
+ REM move data_*.csv %dir%\ohmon_%1.csv
+ move data_*.csv %dir%\vk%init%-%tnum%-%cnum%.csv
  move stat_*.csv %dir%\pipeline_%1.csv
  move conf_*.csv %dir%\
  move fps_*.csv %dir%\
--- Vulkan/testrick.bat
+++ Vulkan/testrick.bat
@@ -2,21 +2,24 @@
 cd %~dp0
 :intro
 CLS
-
 DEL /Q *.csv
+
+ECHO Welcome to the testrick of the Vulkan Application:
 
 SET /p "cubeStartDim=How many cubes to start with (NxNxN)? "
 SET /p "cubeInc=How many cubes shall each test increment with? "
 SET /p "seconds=How many seconds shall the program run for? "
 SET /p "threadCount=How many threads will be used? "
 SET /p "outputFolder=What folder shall the collected data be put in? "
+SET /p "initial=What is your initial [b/c/m]? "
+SET /p "testNum=What number of test run is this (start at 1)? "
 SET /p "testCount=How many times are we running this test? "
 
-ECHO Next time you press ENTER, the tests will begin.
+ECHO Now, next time you press ENTER, the tests will begin.
 PAUSE
 
 CALL :repeat %testCount% for_body
-ECHO Test complete!
+ECHO Test complete! You did it, Morty!
 PAUSE
 
 GOTO :EOF
@@ -30,7 +33,7 @@ REM %6 = outputFolder
 
 SET /A cubeDim=%1*%3+%2%
 
-START "Test #%1" /WAIT AutoRecord.bat %4 %cubeDim% %5 %6
+START "Test #%1" /WAIT AutoRecord.bat %4 %cubeDim% %5 %6 %1 %7 %8
 echo test #%1 done
 REM subroutine for old-fashioned for-loop
 
@@ -42,8 +45,8 @@ SET i=0
 
 :for_loop
 IF %i% LSS %count% (
-	CALL :%func% %i% %cubeStartDim% %cubeInc% %seconds% %threadCount% %outputFolder%
 	SET /A i=%i%+1
+	CALL :%func% %i% %cubeStartDim% %cubeInc% %seconds% %threadCount% %outputFolder% %initial% %testNum%
 	GOTO :for_loop
 ) ELSE (
 	GOTO :EOF
